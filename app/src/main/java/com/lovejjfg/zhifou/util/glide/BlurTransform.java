61af2ed7344a0ccc268f183131d4bac8148ab573
张俊 <zhangjun166@pingan.com.cn>
2016-06-06 16:34:42 +0800

    add BlurTransform.java

---
+++ app/src/main/java/com/lovejjfg/zhifou/util/glide/BlurTransform.java
@@ -0,0 +1,62 @@
+package com.lovejjfg.zhifou.util.glide;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.renderscript.Allocation;
+import android.renderscript.Element;
+import android.renderscript.RenderScript;
+import android.renderscript.ScriptIntrinsicBlur;
+
+import com.bumptech.glide.load.engine.bitmap_recycle.BitmapPool;
+import com.bumptech.glide.load.resource.bitmap.BitmapTransformation;
+
+/**
+ * Created by Joe on 2016-06-06
+ * Email: zhangjun166@pingan.com.cn
+ */
+public class BlurTransform extends BitmapTransformation {
+
+    private RenderScript rs;
+
+    public BlurTransform(Context context) {
+        super( context );
+
+        rs = RenderScript.create( context );
+    }
+
+    @Override
+    protected Bitmap transform(BitmapPool pool, Bitmap toTransform, int outWidth, int outHeight) {
+        Bitmap blurredBitmap = toTransform.copy( Bitmap.Config.ARGB_8888, true );
+
+        // Allocate memory for Renderscript to work with
+        Allocation input = Allocation.createFromBitmap(
+                rs,
+                blurredBitmap,
+                Allocation.MipmapControl.MIPMAP_FULL,
+                Allocation.USAGE_SHARED
+        );
+        Allocation output = Allocation.createTyped(rs, input.getType());
+
+        // Load up an instance of the specific script that we want to use.
+        ScriptIntrinsicBlur script = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs));
+        script.setInput(input);
+
+        // Set the blur radius
+        script.setRadius(10);
+
+        // Start the ScriptIntrinisicBlur
+        script.forEach(output);
+
+        // Copy the output to the blurred bitmap
+        output.copyTo(blurredBitmap);
+
+        toTransform.recycle();
+
+        return blurredBitmap;
+    }
+
+    @Override
+    public String getId() {
+        return "blur";
+    }
+}
