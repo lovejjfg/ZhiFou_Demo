52ec1dd815fbad5a1a3c89d8731df839f6e00280
张俊 <zhangjun166@pingan.com.cn>
2016-07-27 13:41:50 +0800

    add doubleClick method in UIUtils

--- app/src/main/java/com/lovejjfg/zhifou/util/UIUtils.java
+++ app/src/main/java/com/lovejjfg/zhifou/util/UIUtils.java
@@ -4,6 +4,7 @@ import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.res.Resources;
 import android.os.Build;
+import android.os.SystemClock;
 import android.support.v4.view.ViewCompat;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -65,9 +66,10 @@ public class UIUtils {
 
         return hasNavigationBar;
     }
+
     /**
      * @return Whether it is possible for the child view of this layout to
-     *         scroll up. Override this if the child view is a custom view.
+     * scroll up. Override this if the child view is a custom view.
      */
     public boolean canChildScrollUp(View mTarget) {
         if (android.os.Build.VERSION.SDK_INT < 14) {
@@ -82,6 +84,30 @@ public class UIUtils {
         } else {
             return ViewCompat.canScrollVertically(mTarget, -1);
         }
+    }
+
+    /**
+     * 双击事件、多击事件
+     */
+    //存储时间的数组
+    static long[] mHits = new long[2];
+
+    public static boolean doubleClick() {
+        // 双击事件响应
+        /**
+         * arraycopy,拷贝数组
+         * src 要拷贝的源数组
+         * srcPos 源数组开始拷贝的下标位置
+         * dst 目标数组
+         * dstPos 开始存放的下标位置
+         * length 要拷贝的长度（元素的个数）
+         *
+         */
+        //实现数组的移位操作，点击一次，左移一位，末尾补上当前开机时间（cpu的时间）
+        System.arraycopy(mHits, 1, mHits, 0, mHits.length - 1);
+        mHits[mHits.length - 1] = SystemClock.uptimeMillis();
+        //双击事件的时间间隔500ms
+        return mHits[0] >= (SystemClock.uptimeMillis() - 500);
     }
 
 }
