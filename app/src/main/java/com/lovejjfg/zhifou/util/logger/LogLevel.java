e9e36b2ae08690fd4c9e616690f686db510c8a8d
Joe <lovejjfg@163.com>
2016-08-28 11:51:36 +0800

    add Logger

---
+++ app/src/main/java/com/lovejjfg/zhifou/util/logger/LogLevel.java
@@ -0,0 +1,17 @@
+package com.lovejjfg.zhifou.util.logger;
+
+/**
+ * @author Orhan Obut
+ */
+public enum LogLevel {
+
+    /**
+     * Prints all logs
+     */
+    FULL,
+
+    /**
+     * No log will be printed
+     */
+    NONE
+}
